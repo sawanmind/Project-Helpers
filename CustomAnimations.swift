0a191d648c9d864ca3a61842751e4c962baa76a8
Sawan Kumar <sawanmind@gmail.com>
2017-03-19 23:51:12 +0530

    Project Helpers

---
+++ CustomAnimations.swift
@@ -0,0 +1,37 @@
+//
+//  CustomAnimations.swift
+//  bhk
+//
+//  Created by Developer on 3/2/17.
+//  Copyright © 2017 Sawan Kumar. All rights reserved.
+//
+
+import UIKit
+import RAMAnimatedTabBarController
+
+class CustomAnimations: RAMItemAnimation {
+
+    override func playAnimation(_ icon: UIImageView, textLabel: UILabel) {
+        playBounceAnimation(icon)
+        textLabel.textColor = textSelectedColor
+    }
+    
+    override func deselectAnimation(_ icon: UIImageView, textLabel: UILabel, defaultTextColor: UIColor, defaultIconColor: UIColor) {
+        textLabel.textColor = defaultTextColor
+    }
+    
+    override func selectedState(_ icon: UIImageView, textLabel: UILabel) {
+        textLabel.textColor = textSelectedColor
+    }
+    
+    func playBounceAnimation(_ icon : UIImageView) {
+        
+        let bounceAnimation = CAKeyframeAnimation(keyPath: "transform.scale")
+        bounceAnimation.values = [1.0 ,1.4, 0.9, 1.15, 0.95, 1.02, 1.0]
+        bounceAnimation.duration = TimeInterval(duration)
+        bounceAnimation.calculationMode = kCAAnimationCubic
+        
+        icon.layer.add(bounceAnimation, forKey: "bounceAnimation")
+    }
+   
+}
---
+++ Extension.swift
@@ -0,0 +1,109 @@
+//
+//  Extension.swift
+//  Laundary House
+//
+//  Created by BBB Technology on 1/13/17.
+//  Copyright © 2017 BBB Technology. All rights reserved.
+//
+
+import UIKit
+
+
+
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        view.addGestureRecognizer(tap)
+    }
+    
+    func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
+
+extension UIView {
+    
+ 
+    func setShadowWithColor(color: UIColor?, opacity: Float?, offset: CGSize?, radius: CGFloat, viewCornerRadius: CGFloat?) {
+        //layer.shadowPath = UIBezierPath(roundedRect: bounds, cornerRadius: viewCornerRadius ?? 0.0).CGPath
+        layer.shadowColor = color?.cgColor ?? UIColor.black.cgColor
+        layer.shadowOpacity = opacity ?? 1.0
+        layer.shadowOffset = offset ?? CGSize.zero
+        layer.shadowRadius = radius 
+    }
+}
+
+
+
+extension UIImage {
+    
+    func alpha(value:CGFloat)->UIImage
+    {
+        UIGraphicsBeginImageContextWithOptions(size, false, scale)
+        draw(at: CGPoint.zero, blendMode: .normal, alpha: value)
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return newImage!
+        
+    }
+}
+
+
+extension String {
+    var isValidEmailAddress: Bool {
+        let types: NSTextCheckingResult.CheckingType = [.link]
+        let linkDetector = try? NSDataDetector(types: types.rawValue)
+        let range = NSRange(location: 0, length: self.characters.count)
+        let result = linkDetector?.firstMatch(in: self, options: .reportCompletion, range: range)
+        let scheme = result?.url?.scheme ?? ""
+        return scheme == "mailto" && result?.range.length == self.characters.count
+    }
+}
+
+
+extension UIView {
+    
+    func shake(count : Float? = nil,for duration : TimeInterval? = nil,withTranslation translation : Float? = nil) {
+        let animation : CABasicAnimation = CABasicAnimation(keyPath: "transform.translation.x")
+        animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionLinear)
+        
+        animation.repeatCount = count ?? 3
+        animation.duration = (duration ?? 0.5)/TimeInterval(animation.repeatCount)
+        animation.autoreverses = true
+        animation.byValue = translation ?? -5
+        layer.add(animation, forKey: "shake")
+    }
+}
+
+
+extension UIScrollView {
+    
+    // Scroll to a specific view so that it's top is at the top our scrollview
+    func scrollToView(view:UIView, animated: Bool) {
+        if let origin = view.superview {
+            // Get the Y position of your child view
+            let childStartPoint = origin.convert(view.frame.origin, to: self)
+            // Scroll to a rectangle starting at the Y of your subview, with a height of the scrollview
+            self.scrollRectToVisible(CGRect(x:0, y:childStartPoint.y, width:1, height: self.frame.height), animated: animated)
+            
+        }
+    }
+    
+    // Bonus: Scroll to top
+    func scrollToTop(animated: Bool) {
+        let topOffset = CGPoint(x: 0, y: -contentInset.top)
+        setContentOffset(topOffset, animated: animated)
+    }
+    
+    // Bonus: Scroll to bottom
+    func scrollToBottom() {
+        let bottomOffset = CGPoint(x: 0, y: contentSize.height - bounds.size.height + contentInset.bottom)
+        if(bottomOffset.y > 0) {
+            setContentOffset(bottomOffset, animated: true)
+        }
+    }
+    
+}
+
+
+
